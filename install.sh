4c21bddce7a4e2578ca7254ebf2d92812b93c43c
2duo <69870860+2duo@users.noreply.github.com>
2021-04-30 14:22:16 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+echo installing qemu
+sudo pacman -Syyu
+sudo pacman -S base-devel
+cd ~
+mkdir git
+cd git
+sudo pacman -S git 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+echo installing yay (aur helper)
+clear
+makepkg -si
+clear
+cd ..
+sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+sudo pacman -S ebtables iptables
+sudo pacman -Syy
+yay -S --noconfirm --needed libguestfs
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+cd /etc/libvirt
+sudo pacman -S wget
+sudo wget https://raw.githubusercontent.com/2duo/easyqemu/main/libvirtd.conf
+sudo usermod -a -G libvirt $(whoami)
+newgrp libvirt
+sudo systemctl restart libvirtd.service
+echo Confirm that Nested Virtualization is set to Yes:
+
+echo $ systool -m kvm_intel -v | grep nested
+echo    nested              = "Y"
+echo    nested_early_check  = "N"
+echo$ cat /sys/module/kvm_intel/parameters/nested 
+echo Y
+sleep 1
+clear
+sudo modprobe -r kvm_intel
+sudo modprobe kvm_intel nested=1
+echo "options kvm-intel nested=1" | sudo tee /etc/modprobe.d/kvm-intel.conf
+systool -m kvm_intel -v | grep nested
+cat /sys/module/kvm_intel/parameters/nested
+clear
+echo now is qemu install launch now virtual machine manager
