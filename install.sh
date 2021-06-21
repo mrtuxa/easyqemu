77e91407be7f5448257028917cb47729994eaa01
2duo <69870860+2duo@users.noreply.github.com>
2021-06-21 11:31:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
-echo installing qemu
-sudo pacman -Syyu
-sudo pacman -S base-devel
-cd ~
-mkdir git
-cd git
-sudo pacman -S git 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-echo installing yay aka aur helper
-clear
-makepkg -si
-clear
-cd ..
-sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
-sudo pacman -S ebtables iptables
-sudo pacman -Syy
-yay -S --noconfirm --needed libguestfs
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
-cd /etc/libvirt
-sudo rm libvirtd.conf
-sudo pacman -S wget
-sudo wget https://raw.githubusercontent.com/2duo/easyqemu/main/libvirtd.conf
-sudo usermod -a -G libvirt $(whoami)
-newgrp libvirt
-sudo systemctl restart libvirtd.service
-echo Confirm that Nested Virtualization is set to Yes:
+echo installing qemu;
+sudo pacman -Syyu;
+sudo pacman -S base-devel;
+cd ~;
+mkdir git;
+cd git;
+sudo pacman -S git;
+git clone https://aur.archlinux.org/yay.git;
+cd yay;
+echo installing yay aka aur helper;
+clear;
+makepkg -si;
+clear;
+cd ..;
+sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat;
+sudo pacman -S ebtables iptables;
+sudo pacman -Syy;
+yay -S --noconfirm --needed libguestfs;
+sudo systemctl enable libvirtd.service;
+sudo systemctl start libvirtd.service;
+cd /etc/libvirt;
+sudo rm libvirtd.conf;
+sudo pacman -S wget;
+sudo wget https://raw.githubusercontent.com/2duo/easyqemu/main/libvirtd.conf;
+sudo usermod -a -G libvirt $(whoami);
+newgrp libvirt;
+sudo systemctl restart libvirtd.service;
+echo Confirm that Nested Virtualization is set to Yes:;
 
-echo $ systool -m kvm_intel -v | grep nested
-echo    nested              = "Y"
-echo    nested_early_check  = "N"
-echo$ cat /sys/module/kvm_intel/parameters/nested 
-echo Y
-sleep 1
-clear
-sudo modprobe -r kvm_intel
-sudo modprobe kvm_intel nested=1
-echo "options kvm-intel nested=1" | sudo tee /etc/modprobe.d/kvm-intel.conf
-systool -m kvm_intel -v | grep nested
-cat /sys/module/kvm_intel/parameters/nested
-clear
-echo now is qemu installed launch now virtual machine manager
+echo $ systool -m kvm_intel -v | grep nested;
+echo    nested              = "Y";
+echo    nested_early_check  = "N";
+echo$ cat /sys/module/kvm_intel/parameters/nested;
+echo Y;
+sleep 1;
+clear;
+sudo modprobe -r kvm_intel;
+sudo modprobe kvm_intel nested=1;
+echo "options kvm-intel nested=1" | sudo tee /etc/modprobe.d/kvm-intel.conf;
+systool -m kvm_intel -v | grep nested;
+cat /sys/module/kvm_intel/parameters/nested;
+clear;
+echo now is qemu installed launch now virtual machine manager;
